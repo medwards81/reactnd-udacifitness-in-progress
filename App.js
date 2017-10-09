30cf1aff118e06e0856b22aad9f306ccd482430d
Marc Edwards <mdouglasedwards@gmail.com>
2017-10-09 15:17:20 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import AddEntry from './components/AddEntry'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+				<AddEntry />
+				<Text>Hello hello!</Text>
+      </View>
+    );
+  }
+}
